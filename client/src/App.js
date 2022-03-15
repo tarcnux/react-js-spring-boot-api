59417c1ada0ed28342f7be7c6b637727152cb910
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-15 11:37:44 -0300

    Passagem de valores por propriedades

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from './Header';
+import Header2 from './Header2';
 
 export default function App() {
   return (
-    <h1>Aloha mundo!</h1>
+    <>
+      <Header title="React REST client doTatá"/>
+      <Header2>
+        Outro exemplo de passagem de propriedades
+      </Header2>
+      <h1>Aloha REST!</h1>
+    </>
   );
 }
 
---
+++ client/src/Header2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header2({children}) {
+    return (
+        <header>
+            <h1>{children}</h1>
+        </header>
+    )
+}
