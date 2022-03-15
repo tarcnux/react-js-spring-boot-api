27bae216ee09c13aa2926ea0f16ac8fdf2f75b4e
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-15 11:45:36 -0300

    Conceito de estado e imutabilidade

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import Header2 from './Header2';
 
 export default function App() {
+  //Array [valor_original, função de manipulação de valor]
+  const [contador, setContador] = useState(0);
+  
+  function incrementar(){
+    setContador(contador+1);
+  }
+
   return (
     <>
       <Header title="React REST client doTatá"/>
       <Header2>
-        Outro exemplo de passagem de propriedades
+        Contador: {contador}
       </Header2>
       <h1>Aloha REST!</h1>
+      <button onClick={incrementar}>Adicinar</button>
     </>
   );
 }
