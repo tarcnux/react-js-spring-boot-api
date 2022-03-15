534831b23cb799bd4a9b9b496d73721eef5b68db
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-15 10:23:31 -0300

    Adding Server API in Spring Boot 2.6 Java 16

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-js-spring-boot-api
 REST API’s RESTful com React JS, Axios, Postman e Swagger
+
+## Swagger
+http://localhost:8080/swagger-ui.html
+
---
+++ server/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,28 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import br.com.erudio.config.FileStorageConfig;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
+@EnableAutoConfiguration
+@ComponentScan
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+		
+		/*
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		String result = bCryptPasswordEncoder.encode("password");
+		System.out.println("My hash " + result);
+		*/
+	}
+}
