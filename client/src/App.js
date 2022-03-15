d9fa1e543afa0a25f57adaa25303eb5371bb38d6
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-15 11:14:16 -0300

    Clean initial React client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Aloha mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Aloha mundo!</h1>
   );
 }
 
-export default App;
