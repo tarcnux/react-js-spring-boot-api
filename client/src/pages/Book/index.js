4e4947eb37fb9ede464cca42598c5236224bbf76
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-18 10:54:26 -0300

    Cria cabeçalho de Books

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower } from 'react-icons/fi'; //https://feathericons.com/?query=power
+
 import './styles.css';
+
+import logoImage from '../../assets/logo.png';
 
 export default function Book() {
     return (
-        <h1>Book Page</h1>
+       <div className="book-container">
+           <header>
+               <img src={logoImage} alt="TNX Logo" />
+               <span>Bem vindo, <strong>nome</strong>!</span>
+               <Link className="button" to="book/new">Adicionar novo livro</Link>
+               <button>
+                    <FiPower size={18} color="#251fc5" />
+               </button>
+           </header>
+       </div>
     )
 }
