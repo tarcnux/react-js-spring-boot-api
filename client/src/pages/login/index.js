65e803bedbee0696801c16d05848ed8183a06135
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-18 09:30:25 -0300

    Define estrutura HTML do Login

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import './styles.css';
+import logoImage from '../../assets/logo.png';
+import padlockImage from '../../assets/padlock.png';
 
 export default function Login() {
     return (        
-            <h1>Welcome yto the Jungle</h1>        
+            <div className="login-container">
+                <section className="fom">
+                    <img src={logoImage} alt="Erudio Logo" />
+                    <form>
+                        <h1>Access your Account</h1>
+                        <input type="text" placeholder="Username"/>
+                        <input type="password" placeholder="Password" />
+
+                        <button type="submit">Login</button>
+                    </form>
+                </section>
+                <img src={padlockImage} alt="Login" />
+            </div>
     )
 }
