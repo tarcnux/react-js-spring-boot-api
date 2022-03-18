eb69e0ac6193c9b97611d9b57359c08af539704d
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-18 10:22:27 -0300

    Configura Rotas com react-router-dom@5.2.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './global.css';
-import Login from './pages/login';
+import Routes from './routes';
+
 
 export default function App() {
   
   return (
-    <>
-     <Login />
-    </>
+    <Routes />
   );
 }
 
---
+++ client/src/routes.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Login} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
