7b048ca153357dad8e223291a5acbefd03cd8875
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-22 08:18:08 -0300

    Integra a tela de Login à API com Axios

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom";
 import './styles.css';
+
+import api from '../../services/api';
+
+
 import logoImage from '../../assets/logo.png';
 import padlockImage from '../../assets/padlock.png';
 
 export default function Login() {
+
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function login(e) {
+        e.preventDefault();
+        const data = {
+            username, 
+            password
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+
+            history.push('/books');
+        } catch (err) {
+            console.log('username: ' + username);
+            console.log('password: ' + password);
+            alert('Falhou o login, tente novamente!');
+        }
+    }
+
     return (        
             <div className="login-container">
                 <section className="form">
                 <img src={logoImage} alt="TNX Logo" />
-                    <form>
+                    <form onSubmit={login}>
                         <h1>Acessar sua conta</h1>
-                        <input type="text" placeholder="Username"/>
-                        <input type="password" placeholder="Password" />
+                        <input 
+                            type="text" 
+                            placeholder="Username"
+                            value={username}
+                            onChange={e => setUserName(e.target.value)}
+                        />
+                        <input 
+                            type="password" 
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
 
                         <button type="submit" className="button">Login</button>
                     </form>
