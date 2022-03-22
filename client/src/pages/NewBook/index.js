0435c49e7e5681f5e15ba3b36622fa22c184844f
Tarcnux <github@tarcnux.com.br>
2022-03-22 17:54:44 -0300

    Integra a Tela de Cadastr de Livros à API

--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 import logoImage from '../../assets/logo.png';
 
 export default function NewBook() {
+
+    const [id, setId] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    const history = useHistory();
+
+    async function createNewBook(e) {
+        e.preventDefault();
+
+        const data = {
+            title, author, launchDate, price,
+        };
+
+        //Recuperar access token do LocalStorage
+        //const username = localStorage.getItem('username');
+        const accessToken = localStorage.getItem('accessToken');
+
+        const header = {                        
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        };
+
+        try {
+            await api.post('api/book/v1', data, header);
+            history.push('/books');
+
+        } catch (err) {
+            alert('Erro ao tentar salvar o Livro. Tente novamente.');
+        }
+    }
+
     return (
         <div className="new-book-container">
             <div className="content">
@@ -18,12 +55,23 @@ export default function NewBook() {
                         <span>Retornar</span>
                     </Link>
                 </section>
-                <form>
-                    <input type="text" placeholder="Título" />
-                    <input type="text" placeholder="Autor" />
-                    <input type="date" />
-                    <input type="text" placeholder="Preço" />
-
+                <form onSubmit={createNewBook}>
+                    <input  type="text" placeholder="Título" 
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                    />
+                    <input  type="text" placeholder="Autor" 
+                            value={author}
+                            onChange={e => setAuthor(e.target.value)}
+                    />
+                    <input  type="date" 
+                            value={launchDate}
+                            onChange={e => setLaunchDate(e.target.value)}
+                    />
+                    <input  type="text" placeholder="Preço" 
+                            value={price}
+                            onChange={e => setPrice(e.target.value)}
+                    />
                     <button type="submit" className="button">Adicionar</button>
                 </form>
             </div>
