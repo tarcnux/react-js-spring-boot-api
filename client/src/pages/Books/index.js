a7026ecea9e646faa6dc38a577a92f338599d5ce
Tarcnux <github@tarcnux.com.br>
2022-03-23 17:13:22 -0300

    Implementa atualização de livros e carregamento de páginas

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -11,6 +11,7 @@ import logoImage from '../../assets/logo.png';
 export default function Books() {
 
      const [books, setBooks] = useState([]);
+     const [page, setPage] = useState(0);
 
     //Recuperar access token do LocalStorage
     const username = localStorage.getItem('username');
@@ -23,42 +24,63 @@ export default function Books() {
          history.push('/')
     }
     
+    async function editBook(id) {
+         try {
+               history.push(`book/new/${id}`);
+         } catch(error) {
+              alert(`Não foi possível editar o livro: ${id}`);
+         }
+    }
+
     async function deleteBook(id) {
      console.log(`Excluíndo livro ${id}...`);
           try {
-               await api.delete(`api/book/v1/${id}`, {                        
+               await api.delete(`api/book/v1/${id}`, {
                     headers: {
                     Authorization: `Bearer ${accessToken}`
                     }
                });
 
                setBooks(books.filter(book => book.id !== id));
-          } catch(err) {
-               alert('Não foi possível apagar!');
+          } catch(error) {
+               alert(`Não foi possível apagar o livro: ${id}`);
           }
      }
-    
-     //Carrega os dados no carregamento da tela
-    useEffect(() => {
-         console.log('Carregando livros');
-          api.get('api/book/v1', {                        
+
+     async function fetchMoreBooks() {
+          console.log(`Carregando mais livros da página: ${page}`);
+
+          const response = await api.get('api/book/v1', {                        
                headers: {
-               Authorization: `Bearer ${accessToken}`
+                    Authorization: `Bearer ${accessToken}`
                },
                params: {
-                    page: 0,
+                    page,
                     limit: 4,
                     direction: 'asc'
                }
-          }).then(response => {setBooks(response.data._embedded.bookVoes)});
-    }, []);
+          });
+          
+          if('_embedded' in response.data){
+               setBooks([...books, ...response.data._embedded.bookVoes]);
+               setPage(page+1);
+          }
+     };
+     
+    
+     //Carrega os dados no carregamento da tela
+     useEffect(() => {
+          console.log('Carregando livros');          
+          fetchMoreBooks();
+     
+     }, []);
 
-    return (
+     return (
        <div className="book-container">
            <header>
                <img src={logoImage} alt="TNX Logo" />
                <span>Bem vindo, <strong>{username.toUpperCase()}</strong>!</span>
-               <Link className="button" to="book/new">Adicionar novo livro</Link>
+               <Link className="button" to="book/new/0">Adicionar novo livro</Link>
                <button onClick={() => logout()}>
                     <FiPower size={18} color="#251fc5" />
                </button>
@@ -76,7 +98,7 @@ export default function Books() {
                          <strong>Data de Lançamento</strong>
                          <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
      
-                         <button>
+                         <button onClick={() => editBook(book.id)}>
                               <FiEdit size={18} color="#251fc5" />
                          </button>
      
@@ -87,6 +109,7 @@ export default function Books() {
                ))} 
                               
            </ul>
+           <button className="button" onClick={fetchMoreBooks}>Carregar mais livros...</button>
        </div>
     )
 }
--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -9,18 +9,52 @@ import logoImage from '../../assets/logo.png';
 
 export default function NewBook() {
 
-    //const [id, setId] = useState(null);
+    const [id, setId] = useState(null);
     const [author, setAuthor] = useState('');
     const [launchDate, setLaunchDate] = useState('');
     const [price, setPrice] = useState('');
     const [title, setTitle] = useState('');
 
+    //Parâmetro de URL, o mesmo que em routes.js :bookId
+    const {bookId} = useParams(); //
+
     //Recuperar access token do LocalStorage
     //const username = localStorage.getItem('username');
     const accessToken = localStorage.getItem('accessToken');
+
+    useEffect(() => {
+        async function loadBook(id){
+            if (bookId === '0') return;
+
+            console.log(`Carregar livro Id: ${bookId}`);
+
+            try {
+                    const response = await api.get(`api/book/v1/${bookId}`,{
+                        headers: {
+                        Authorization: `Bearer ${accessToken}`
+                        }
+                });
+
+                let ajusteDate = response.data.launchDate.split('T',10)[0];
+                setId(response.data.id);
+                setTitle(response.data.title);
+                setAuthor(response.data.author);
+                setPrice(response.data.price);
+                setLaunchDate(ajusteDate);                
+
+            } catch(error) {
+                alert(`Erro ao tentar carregar o livro ${id}. Tente novamente!`);
+                history.push('/books');
+            }
+        };    
+        
+        loadBook();
+        
+    },[bookId]);
+
     const history = useHistory();
 
-    async function createNewBook(e) {
+    async function saveOrUpdate(e) {
         e.preventDefault();
 
         const data = {
@@ -34,7 +68,14 @@ export default function NewBook() {
         };
 
         try {
-            await api.post('api/book/v1', data, header);
+
+            if (bookId === '0') {
+                await api.post('api/book/v1', data, header);
+            } else {
+                data.id = id;
+                await api.put('api/book/v1', data, header);
+            }
+
             history.push('/books');
 
         } catch (err) {
@@ -48,13 +89,14 @@ export default function NewBook() {
                 <section className="form">
                     <img src={logoImage} alt="TNX Logo" />
                     <h1>Adicionar Novo Livro</h1>
-                    <p>Preencha os dados e clique 'Adicionar'</p>
+                    {/* <code>DEBUG: {bookId}</code> */}
+                    <p>Preencha os dados e clique 'Adicionar'</p>                                        
                     <Link className="back-link" to="/books">
                         <FiArrowLeft size={16} color="251fc5" />
                         <span>Retornar</span>
                     </Link>
                 </section>
-                <form onSubmit={createNewBook}>
+                <form onSubmit={saveOrUpdate}>
                     <input  type="text" placeholder="Título" 
                             value={title}
                             onChange={e => setTitle(e.target.value)}
@@ -71,7 +113,7 @@ export default function NewBook() {
                             value={price}
                             onChange={e => setPrice(e.target.value)}
                     />
-                    <button type="submit" className="button">Adicionar</button>
+                    <button type="submit" className="button">{bookId==='0'?'Adicionar':'Salvar'}</button>
                 </form>
             </div>
         </div>
